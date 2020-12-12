1751607fdcf440c97fd38b93fa03eac3a976a7c8
otus <otusex@gmail.com>
2020-12-12 22:07:14 +0200

    change

--- wdo.sh
+++ wdo.sh
@@ -11,7 +11,8 @@ yum check-update
 yum install ntp -y 
 
 
-systemctl start ntpd && systemctl enable ntpd
+systemctl start ntpd 
+systemctl enable ntpd
 
 yum install audit audispd-plugins policycoreutils-python nginx -y
 
@@ -23,7 +24,6 @@ worker_processes auto;
 error_log /var/log/nginx/error.log;
 pid /run/nginx.pid;
 
-# Load dynamic modules. See /usr/share/doc/nginx/README.dynamic.
 include /usr/share/nginx/modules/*.conf;
 
 events {
@@ -51,9 +51,6 @@ http {
     include             /etc/nginx/mime.types;
     default_type        application/octet-stream;
 
-    # Load modular configuration files from the /etc/nginx/conf.d directory.
-    # See http://nginx.org/en/docs/ngx_core_module.html#include
-    # for more information.
     include /etc/nginx/conf.d/*.conf;
 
     server {
@@ -77,159 +74,59 @@ http {
         }
     }
 
-# Settings for a TLS enabled server.
-#
-#    server {
-#        listen       443 ssl http2 default_server;
-#        listen       [::]:443 ssl http2 default_server;
-#        server_name  _;
-#        root         /usr/share/nginx/html;
-#
-#        ssl_certificate "/etc/pki/nginx/server.crt";
-#        ssl_certificate_key "/etc/pki/nginx/private/server.key";
-#        ssl_session_cache shared:SSL:1m;
-#        ssl_session_timeout  10m;
-#        ssl_ciphers HIGH:!aNULL:!MD5;
-#        ssl_prefer_server_ciphers on;
-#
-#        # Load configuration files for the default server block.
-#        include /etc/nginx/default.d/*.conf;
-#
-#        location / {
-#        }
-#
-#        error_page 404 /404.html;
-#            location = /40x.html {
-#        }
-#
-#        error_page 500 502 503 504 /50x.html;
-#            location = /50x.html {
-#        }
-#    }
 
 }
 EOF
 
 cat << EOF > /etc/rsyslog.conf
-# rsyslog configuration file
 
-# For more information see /usr/share/doc/rsyslog-*/rsyslog_conf.html
-# If you experience problems, see http://www.rsyslog.com/doc/troubleshoot.html
-
-#### MODULES ####
-
-# The imjournal module bellow is now used as a message source instead of imuxsock.
-$ModLoad imuxsock # provides support for local system logging (e.g. via logger command)
-$ModLoad imjournal # provides access to the systemd journal
-#$ModLoad imklog # reads kernel messages (the same are read from journald)
-#$ModLoad immark  # provides --MARK-- message capability
-
-# Provides UDP syslog reception
-#$ModLoad imudp
-#$UDPServerRun 514
-
-# Provides TCP syslog reception
-#$ModLoad imtcp
-#$InputTCPServerRun 514
+$ModLoad imuxsock 
+$ModLoad imjournal 
 
 
-#### GLOBAL DIRECTIVES ####
-
-# Where to place auxiliary files
 $WorkDirectory /var/lib/rsyslog
 
-# Use default timestamp format
 $ActionFileDefaultTemplate RSYSLOG_TraditionalFileFormat
 
-# File syncing capability is disabled by default. This feature is usually not required,
-# not useful and an extreme performance hit
-#$ActionFileEnableSync on
 
-# Include all config files in /etc/rsyslog.d/
 $IncludeConfig /etc/rsyslog.d/*.conf
 
-# Turn off message reception via local log socket;
-# local messages are retrieved through imjournal now.
 $OmitLocalLogging on
 
-# File to store the position in the journal
 $IMJournalStateFile imjournal.state
 
 
-#### RULES ####
-
-# Log all kernel messages to the console.
-# Logging much else clutters up the screen.
-#kern.*                                                 /dev/console
-
-# Log anything (except mail) of level info or higher.
-# Don't log private authentication messages!
 *.info;mail.none;authpriv.none;cron.none                /var/log/messages
 
-# The authpriv file has restricted access.
 authpriv.*                                              /var/log/secure
 
-# Log all the mail messages in one place.
 mail.*                                                  -/var/log/maillog
 
-
-# Log cron stuff
 cron.*                                                  /var/log/cron
 
-# Everybody gets emergency messages
 *.emerg                                                 :omusrmsg:*
 
-# Save news errors of level crit and higher in a special file.
 uucp,news.crit                                          /var/log/spooler
 
-# Save boot messages also to boot.log
 local7.*                                                /var/log/boot.log
 
-
-# ### begin forwarding rule ###
-# The statement between the begin ... end define a SINGLE forwarding
-# rule. They belong together, do NOT split them. If you create multiple
-# forwarding rules, duplicate the whole block!
-# Remote Logging (we use TCP for reliable delivery)
-#
-# An on-disk queue is created for this action. If the remote host is
-# down, messages are spooled to disk and sent when it is up again.
-#$ActionQueueFileName fwdRule1 # unique name prefix for spool files
-#$ActionQueueMaxDiskSpace 1g   # 1gb space limit (use as much as possible)
-#$ActionQueueSaveOnShutdown on # save messages to disk on shutdown
-#$ActionQueueType LinkedList   # run asynchronously
-#$ActionResumeRetryCount -1    # infinite retries if host is down
-# remote host is: name/ip:port, e.g. 192.168.0.1:514, port optional
-#*.* @@remote-host:514
-# ### end of the forwarding rule ###
 *.* @192.168.10.23
 
 EOF
 
 
 cat << EOF > /etc/audit/rules.d/audit.rules
-## First rule - delete all
 -D
 
-## Increase the buffers to survive stress events.
-## Make this bigger for busy systems
 -b 8192
 
-
-## Set failure mode to syslog
 -f 1
-
-
 -w /etc/nginx/nginx.conf -p wa -k web_config_changed
 -w /etc/nginx/conf.d/ -p wa -k web_config_changed
 EOF
 
 
 cat << EOF > /etc/audisp/audisp-remote.conf
-#
-# This file controls the configuration of the audit remote 
-# logging subsystem, audisp-remote.
-#
 
 remote_server = 192.168.10.23
 port = 60
@@ -262,9 +159,6 @@ overflow_action = syslog
 EOF
 
 cat << EOF > au-remote.conf
-# This file controls the audispd data path to the
-# remote event logger. This plugin will send events to
-# a remote machine (Central Logger).
 
 active = yes
 direction = out
@@ -275,9 +169,6 @@ format = string
 EOF
 
 cat << EOF > /etc/audit/auditd.conf
-#
-# This file controls the configuration of the audit daemon
-#
 
 local_events = yes
 write_logs = no
@@ -316,13 +207,18 @@ EOF
 
 
 
-sudo chown root:root /etc/audit/auditd.conf; sudo chmod 640 /etc/audit/auditd.conf;
-sudo chown root:root /etc/nginx/nginx.conf; sudo chmod 644 /etc/nginx/nginx.conf;
-sudo chown root:root /etc/rsyslog.conf; sudo chmod 644 /etc/rsyslog.conf;
-sudo chown root:root /etc/audit/rules.d/audit.rules; sudo chmod 644 /etc/audit/rules.d/audit.rules;
-sudo chown root:root /etc/audisp/audisp-remote.conf; sudo chmod 644 /etc/audisp/audisp-remote.conf;
+sudo chown root:root /etc/audit/auditd.conf
+sudo chmod 640 /etc/audit/auditd.conf
+sudo chown root:root /etc/nginx/nginx.conf
+sudo chmod 644 /etc/nginx/nginx.conf
+sudo chown root:root /etc/rsyslog.conf
+sudo chmod 644 /etc/rsyslog.conf
+sudo chown root:root /etc/audit/rules.d/audit.rules
+sudo chmod 644 /etc/audit/rules.d/audit.rules
+sudo chown root:root /etc/audisp/audisp-remote.conf
+sudo chmod 644 /etc/audisp/audisp-remote.conf
 sudo chown root:root /etc/audisp/plugins.d/au-remote.conf
-sudo chmod 644 /etc/audisp/plugins.d/au-remote.conf;
+sudo chmod 644 /etc/audisp/plugins.d/au-remote.conf
 systemctl enable nginx 
 systemctl start nginx
 systemctl restart rsyslog
